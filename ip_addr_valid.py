2ee3e532e1d62d2a113354a6f3a651e53738d04c
fdeswardt <fdeswardt@paloaltonetworks.com>
2019-12-02 12:46:10 -0800

    added comments and OSX specific TXT file location

--- ip_addr_valid.py
+++ ip_addr_valid.py
@@ -4,12 +4,32 @@ import sys
 def ip_addr_valid(list):
 
     for ip in list:
+        # remove the trailing \n produced by the readlines() function
         ip = ip.rstrip("\n")
+
+        # split the IP address into a list of 4 octets
         octet_list = ip.split('.')
-        
-        if (len(octet_list) == 4) and (1 <= int(octet_list[0]) <= 223) and (int(octet_list[0]) != 127) and (int(octet_list[0]) != 169 or int(octet_list[1]) != 254) and (0 <= int(octet_list[1]) <= 255 and 0 <= int(octet_list[2]) <= 255 and 0 <= int(octet_list[3]) <= 255):
+
+        # check that IP is not in reserved ranges
+        # See https://en.wikipedia.org/wiki/Reserved_IP_addresses for details
+        #
+        # check IP is not: - multicast: 224.0.0.0/4 -or- reserved for future use: 240.0.0.0/4
+        # check IP is not Link-Local 169.254.0.0/16
+        # check IP is not loopback 127.0.0.0/8
+        # check IP is not broadcast of 255.255.255.255
+        if (len(octet_list) == 4) \
+                and (1 <= int(octet_list[0]) <= 223) \
+                and (int(octet_list[0]) != 169 \
+                     or int(octet_list[1]) != 254) \
+                and (int(octet_list[0]) != 127) \
+                and (0 <= int(octet_list[1]) <= 255 \
+                     and 0 <= int(octet_list[2]) <= 255 \
+                     and 0 <= int(octet_list[3]) <= 255):
+
+            # If octet is valid then continue for-loop to next IP in list
             continue
-             
+
         else:
+            # If octet is not valid then exit with message
             print('\n* There was an invalid IP address in the file: {} :(\n'.format(ip))
             sys.exit()
--- ip_file_valid.py
+++ ip_file_valid.py
@@ -1,13 +1,13 @@
 import os.path
 import sys
 
-#Checking IP address file and content validity
+# Checking IP address file and content validity
 def ip_file_valid():
 
-    #Prompting user for input
-    ip_file = input("\n# Enter IP file path and name (e.g. D:\MyApps\myfile.txt): ")
+    # Prompting user for input
+    ip_file = input("\n# Enter IP file path and name (e.g. ~/device-ips.txt): ")
 
-    #Changing exception message
+    # Changing exception message
     if os.path.isfile(ip_file) == True:
         print("\n* IP file is valid :)\n")
     
@@ -15,16 +15,17 @@ def ip_file_valid():
         print("\n* File {} does not exist :( Please check and try again.\n".format(ip_file))
         sys.exit()
 
-    #Open user selected file for reading (IP addresses file)
+    # Open user selected file for reading (IP addresses file)
     selected_ip_file = open(ip_file, 'r')
     
-    #Starting from the beginning of the file
+    # Starting from the beginning of the file
     selected_ip_file.seek(0)
     
-    #Reading each line (IP address) in the file
+    # Reading each line (IP address) in the file
     ip_list = selected_ip_file.readlines()
     
     #Closing the file
     selected_ip_file.close()
-        
+
+    # return the list of device IPs read from TXT file as a list
     return ip_list
