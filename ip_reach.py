5c0032680ecefd9a4a76bc856f69568e4f262b0f
fdeswardt <fdeswardt@paloaltonetworks.com>
2019-12-09 11:12:41 -0800

    add logic to check OS and use correct ping parameter. also changed from subprocess.call() to subprocess.run() to support latest Python docs guidance

--- ip_reach.py
+++ ip_reach.py
@@ -1,6 +1,10 @@
 import sys
 import subprocess
 
+# Import name from os to check OS type for correct usage of ping
+from os import name
+
+
 #Checking IP reachability
 def ip_reach(list):
 
@@ -10,9 +14,24 @@ def ip_reach(list):
         # Sending only 2 ICMP packets with "-n 2"
         # Redirecting stdout and stderr to DEVNULL to supress all output from ping as we are only
         #   interested in the return value being 0 if succesfull or !=0 if failed
-        ping_reply = subprocess.call('ping %s -c 2' % (ip,), stdout=subprocess.DEVNULL, \
-                                     stderr=subprocess.DEVNULL, shell=True)
-        
+
+        # for windows
+        # use "-n 2" parameter
+        if name == 'nt':
+            ping_reply = subprocess.run('ping %s -n 2' % (ip), stdout=subprocess.DEVNULL, \
+                                         stderr=subprocess.DEVNULL)
+
+        # for mac and linux
+        # use "-c 2" parameter
+        elif name == 'posix':
+            ping_reply = subprocess.run('ping %s -c 2' % (ip), stdout=subprocess.DEVNULL, \
+                                         stderr=subprocess.DEVNULL, shell=True)
+
+        # unknown OS
+        else:
+            print(f"Unknown OS \'{name}\', and not able to correctly ping targets, please run on supported OS")
+            sys.exit()
+
         if ping_reply == 0:
             print("\n* {} is reachable :)\n".format(ip))
 
